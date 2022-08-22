5f59deed4479d5f932c3f0cf4ce7be18bcfdc30e
Linet Oliech <oliechlin@gmail.com>
2022-08-22 17:35:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') {
+    return (`This Saturday, I want to ${defaultActivity}!`);
+}
+saturdayFun(); 
+
+function mondayWork(defaultActivity = 'go to the office') {
+    return `This Monday, I will ${defaultActivity}.`;
+}
+mondayWork();
+
+function wrapAdjective (verb="*") {
+    return function(adjective){
+    return `You are ${verb}${adjective}${verb}!`
+    }  
+}
+wrapAdjective();
+
